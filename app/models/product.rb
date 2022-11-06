d61d7ce86d4221f726204805dd5a5438677465c6
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-11-06 12:58:07 +0300

    add code to the products file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,19 @@
 class Product < ActiveRecord::Base
+
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(product: self, user: user, star_rating: star_rating, comment: comment)
+    end
+
+    def print_all_reviews
+        self.reviews.each do |review|
+            puts "Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+        end
+        
+        nil
+    end
     
 end
+
