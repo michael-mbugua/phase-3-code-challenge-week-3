6d48fbdd2490b0f726f020e15c7dab66d752d3ad
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-11-06 13:00:18 +0300

    initial code for review file

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ActiveRecord::Base
+    belongs_to:user 
+    belongs_to:product
+
+    def print_review
+    puts "Review for #{self.product.name} by #{self.product.name}: #{self.star_rating}. #{self.comment}"
+
+    end
+end
